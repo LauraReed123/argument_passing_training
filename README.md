7cff8961bbba1a9c40887c5807dfde17d50b99a0
LauraReed123 <laura.reed@nhsbt.nhs.uk>
2023-01-11 16:39:48 +0000

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # argument_passing_training
+
+**Argument vs Parameter** 
+
+Argument passing is when the a value is passed into the function at the time of running the program. 
+Parameters are inside functions and procedures. 
--- python_arguments.py
+++ python_arguments.py
@@ -4,3 +4,25 @@
 # Created on 11/01/2023
 #
 # 
+
+## Example of a Parameter - used inside the parentheses 
+
+# def my_function (fname):
+#     print("First name is: " + fname)
+
+# my_function("Laura")
+# my_function("Nick")
+
+## There can be more than one parameter required by the fuction 
+
+print("Enter 2 numbers to be added together ")
+num1 = int(input("Enter the first number then press enter: "))
+num2 = int(input("Enter the second number then press enter: "))
+#sum = num1 + num2
+
+def sum_two_numbers (num1, num2):
+    sum = num1 + num2
+    print(f"The sum of the numbers is {sum}")
+
+sum_two_numbers(num1, num2)
+
